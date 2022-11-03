cf161ccd505ec5d4becd6e37450443b58848a7b0
Daiane <99364026+DaianedeOliveira@users.noreply.github.com>
2022-11-03 19:53:54 -0300

    Update CondicionalTernaria.java

--- src/br/com/Atividade/CondicionalTernaria.java
+++ src/br/com/Atividade/CondicionalTernaria.java
@@ -5,8 +5,8 @@ public class CondicionalTernaria {
 	public static void main(String[] args) {
     //Condicional Ternária
 		int nota = 6;
-		String resultado = nota >=6  && nota <8 ? "Recuperação" : "Reprovado";
-		System.out.println(resultado);
+		String resultados = nota >=6  && nota <8 ? "Recuperação" : "Reprovado";
+		System.out.println(resultados);
 		
 	}	
 	
